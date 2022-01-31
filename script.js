74de277e49fffcfa628730c917cb32f53f5ee6d5
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-01-30 23:41:10 -0300

    Projeto pronto, com todos os requisitos obrigatórios.

--- script.js
+++ script.js
@@ -4,209 +4,22 @@ let tem_prato = false;
 let tem_refri = false;
 let tem_sobremesa = false;
 
-let precoPrato;
-let precoRefri;
-let precoSobremesa;
-let total;
-let totalArredondado;
+let precoPrato = null;
+let precoRefri = null;
+let precoSobremesa = null;
+let total = null;
+let totalArredondado = null;
 
-let prato;
-let refrigerante;
-let sobremesa;
+let pratoSelecionado = null;
+let refriSelecionado = null;
+let sobremesaSelecionada = null;
 
 let mensagem;
 
-//Pratos//
-
-/*function prato1 () {
-    document.getElementById("prato_1").style.borderColor = "#32B72F";
-    document.getElementById("prato_2").style.borderColor = "white";
-    document.getElementById("prato_3").style.borderColor = "white";
-    document.getElementById("prato_4").style.borderColor = "white";
-
-    tem_prato = true;
-    prato = "Frango Yin Yang";
-    precoPrato = 14.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function prato2 () {
-    document.getElementById("prato_1").style.borderColor = "white";
-    document.getElementById("prato_2").style.borderColor = "#32B72F";
-    document.getElementById("prato_3").style.borderColor = "white";
-    document.getElementById("prato_4").style.borderColor = "white";
-
-    tem_prato = true;
-    prato = "Hamburguer";
-    precoPrato = 18.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function prato3 () {
-    document.getElementById("prato_1").style.borderColor = "white";
-    document.getElementById("prato_2").style.borderColor = "white";
-    document.getElementById("prato_3").style.borderColor = "#32B72F";
-    document.getElementById("prato_4").style.borderColor = "white";
-
-    tem_prato = true;
-    prato = "Pizza Marguerita";
-    precoPrato = 20.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function prato4 () {
-    document.getElementById("prato_1").style.borderColor = "white";
-    document.getElementById("prato_2").style.borderColor = "white";
-    document.getElementById("prato_3").style.borderColor = "white";
-    document.getElementById("prato_4").style.borderColor = "#32B72F";
-
-    tem_prato = true;
-    prato = "Lasanha Bolonhesa";
-    precoPrato = 16
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-//Bebidas//
-
-function refri1 () {
-    document.getElementById("refri_1").style.borderColor = "#32B72F";
-    document.getElementById("refri_2").style.borderColor = "white";
-    document.getElementById("refri_3").style.borderColor = "white";
-    document.getElementById("refri_4").style.borderColor = "white";
-
-    tem_refri = true;
-    refrigerante = "Coca-Cola";
-    precoRefri = 4.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function refri2 () {
-    document.getElementById("refri_1").style.borderColor = "white";
-    document.getElementById("refri_2").style.borderColor = "#32B72F";
-    document.getElementById("refri_3").style.borderColor = "white";
-    document.getElementById("refri_4").style.borderColor = "white";
-
-    tem_refri = true;
-    refrigerante = "Pepsi";
-    precoRefri = 4.50;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function refri3 () {
-    document.getElementById("refri_1").style.borderColor = "white";
-    document.getElementById("refri_2").style.borderColor = "white";
-    document.getElementById("refri_3").style.borderColor = "#32B72F";
-    document.getElementById("refri_4").style.borderColor = "white";
-
-    tem_refri = true;
-    refrigerante = "Guaraná";
-    precoRefri = 4.00;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function refri4 () {
-    document.getElementById("refri_1").style.borderColor = "white";
-    document.getElementById("refri_2").style.borderColor = "white";
-    document.getElementById("refri_3").style.borderColor = "white";
-    document.getElementById("refri_4").style.borderColor = "#32B72F";
-
-    tem_refri = true;
-    refrigerante = "Fanta";
-    precoRefri = 4.30;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-//sobremesas//
-
-function sobremesa1 () {
-    document.getElementById("sobremesa_1").style.borderColor = "#32B72F";
-    document.getElementById("sobremesa_2").style.borderColor = "white";
-    document.getElementById("sobremesa_3").style.borderColor = "white";
-    document.getElementById("sobremesa_4").style.borderColor = "white";
-
-    tem_sobremesa = true;
-    sobremesa = "Torta Alemã";
-    precoSobremesa = 8.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function sobremesa2 () {
-    document.getElementById("sobremesa_1").style.borderColor = "white";
-    document.getElementById("sobremesa_2").style.borderColor = "#32B72F";
-    document.getElementById("sobremesa_3").style.borderColor = "white";
-    document.getElementById("sobremesa_4").style.borderColor = "white";
-
-    tem_sobremesa = true;
-    sobremesa = "Açaí com Frutas";
-    precoSobremesa = 12.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function sobremesa3 () {
-    document.getElementById("sobremesa_1").style.borderColor = "white";
-    document.getElementById("sobremesa_2").style.borderColor = "white";
-    document.getElementById("sobremesa_3").style.borderColor = "#32B72F";
-    document.getElementById("sobremesa_4").style.borderColor = "white";
-
-    tem_sobremesa = true;
-    sobremesa = "Red Velvet";
-    precoSobremesa = 9.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}
-
-function sobremesa4 () {
-    document.getElementById("sobremesa_1").style.borderColor = "white";
-    document.getElementById("sobremesa_2").style.borderColor = "white";
-    document.getElementById("sobremesa_3").style.borderColor = "white";
-    document.getElementById("sobremesa_4").style.borderColor = "#32B72F";
-
-    tem_sobremesa = true;
-    sobremesa = "Pudim";
-    precoSobremesa = 7.90;
-
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
-}*/
-
 //Prato//
 
-function escolherPrato(opcaoPrato) {
+function escolherPrato(opcaoPrato, pratoEscolhido, precoPratoEscolhido) {
     const selecionado = document.querySelector(".prato-2.selecionado");
-    console.log(selecionado);
     if(selecionado !== null){
     selecionado.classList.remove("selecionado");
     }
@@ -214,18 +27,28 @@ function escolherPrato(opcaoPrato) {
     const prato = document.querySelector("#"+opcaoPrato);
     prato.classList.toggle("selecionado");
 
-    tem_prato = true;
+    pratoSelecionado = pratoEscolhido;
+    precoPrato = precoPratoEscolhido
+    
 
-    if (tem_prato && tem_refri && tem_sobremesa) {
-        ativarBotão()
-    }
+    document.querySelector("#icon-prato1").style.display = "none";
+    document.querySelector("#icon-prato2").style.display = "none";
+    document.querySelector("#icon-prato3").style.display = "none";
+    document.querySelector("#icon-prato4").style.display = "none";
+    const img = prato.getElementsByClassName("icon-verde")[0];
+    img.style.display = "block";
+   
+    
+   tem_prato = true;
+   if (tem_prato && tem_refri && tem_sobremesa) {
+       ativarBotão()
+   }
 }
 
 //Bebidas//
 
-function escolherBebida(opcaoBebida) {
+function escolherBebida(opcaoBebida, bebidaEscolhida, precoRefriEscolhido) {
     const selecionado = document.querySelector(".bebida-2.selecionado");
-    console.log(selecionado);
     if(selecionado !== null){
     selecionado.classList.remove("selecionado");
     }
@@ -233,8 +56,17 @@ function escolherBebida(opcaoBebida) {
     const bebida = document.querySelector("#"+opcaoBebida);
     bebida.classList.toggle("selecionado");
 
-    tem_refri = true;
+    refriSelecionado = bebidaEscolhida;
+    precoRefri = precoRefriEscolhido;
 
+    document.querySelector("#icon-bebida1").style.display = "none";
+    document.querySelector("#icon-bebida2").style.display = "none";
+    document.querySelector("#icon-bebida3").style.display = "none";
+    document.querySelector("#icon-bebida4").style.display = "none";
+    const img = bebida.getElementsByClassName("icon-verde")[0];
+    img.style.display = "block";
+
+    tem_refri = true;
     if (tem_prato && tem_refri && tem_sobremesa) {
         ativarBotão()
     }
@@ -242,18 +74,33 @@ function escolherBebida(opcaoBebida) {
 
 //Sobremesa//
 
-function escolherSobremesa(opcaoSobremesa) {
+function escolherSobremesa(opcaoSobremesa, sobremesaEscolhida, precoSobremesaEscolhida) {
+
+    //desselecionar a borda  ao  clicar em outra opçao//
     const selecionado = document.querySelector(".sobremesa-2.selecionado");
     console.log(selecionado);
     if(selecionado !== null){
     selecionado.classList.remove("selecionado");
     }
+
+    sobremesaSelecionada = sobremesaEscolhida;
+    precoSobremesa = precoSobremesaEscolhida;
     
+    //add as bordas verdes//
     const sobremesa = document.querySelector("#"+opcaoSobremesa);
     sobremesa.classList.toggle("selecionado");
 
-    tem_sobremesa = true;
+    document.querySelector("#icon-sobremesa1").style.display = "none";
+    document.querySelector("#icon-sobremesa2").style.display = "none";
+    document.querySelector("#icon-sobremesa3").style.display = "none";
+    document.querySelector("#icon-sobremesa4").style.display = "none";
+    const img = sobremesa.getElementsByClassName("icon-verde")[0];
+    img.style.display = "block";
 
+    
+
+    //ativar botão de fechar pedido//
+    tem_sobremesa = true;
     if (tem_prato && tem_refri && tem_sobremesa) {
         ativarBotão()
     }
@@ -278,7 +125,7 @@ function finalizarPedido() {
     let nome= prompt("Pode nos informar seu nome?");
     let endereço= prompt("Nos informe endereço para entrega")
 
-    mensagem = `Olá, gostaria de fazer um pedido: \n\n- Prato: ${prato} \n- Bebida: ${refrigerante} \n- Sobremesa: ${sobremesa} \n\n Total: ${totalArredondado} \n\n Nome: ${nome} \n Endereço de destino: R$${endereço}`;
+    mensagem = `Olá, gostaria de fazer um pedido: \n\n- Prato: ${pratoSelecionado} \n- Bebida: ${refriSelecionado} \n- Sobremesa: ${sobremesaSelecionada} \n\n Total: R$${totalArredondado} \n\n Nome: ${nome} \n Endereço de destino: ${endereço}`;
 
     window.open("https://wa.me/5524999642965?text=" + encodeURI (mensagem));
 }
